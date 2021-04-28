a22b6b91d6469fdc7413e57d4cc970d65d770fbb
Cloud User <centos@ecs-c534-osii-cts.novalocal>
2021-04-28 07:30:04 +0000

    Adding main.tf and variables.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "osandarajapakse"
+
+    workspaces {
+      name = "udemy_test_osanda"
+    }
+  }
+}
+
+provider "aws" {
+	region = "us-east-1"
+	access_key = var.access_key 
+	secret_key = var.secret_key
+}
+
+resource "aws_instance" "backend_ec2" {
+	ami = "ami-0742b4e673072066f"
+	instance_type = "t2.nano"
+}
